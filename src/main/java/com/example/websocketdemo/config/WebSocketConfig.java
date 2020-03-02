6640e3f41ef1b6e87f4f4a7d1236160a0bf657ac
Susan Tabassum <sttabassum93@gmail.com>
2020-03-01 23:17:01 -0500

    Initial push

---
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package com.example.websocketdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{ //what does this stuff even do?
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) { //TODO: look up stomp and sockjs
+        registry.addEndpoint("/ws").withSockJS();
+    }
+}
