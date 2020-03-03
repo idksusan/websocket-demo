0de9f52c40012aff92add9fdeb0ad08ec6dea043
Susan Tabassum <sttabassum93@gmail.com>
2020-03-02 23:28:42 -0500

    Still working on js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -31,3 +31,19 @@ function connect(event) {
     event.preventDefault();
 }
 
+function onConnected() {
+    // Subscribe to the Public Topic
+    stompClient.subscribe('/topic/public', onMessageReceived);
+
+    // Tell your username to the server
+    stompClient.send("/app/chat.addUser",
+        {},
+        JSON.stringify({sender: username, type: 'JOIN'})
+    )
+    connectingElement.classList.add('hidden');
+}
+
+function onError(error) {
+    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    connectingElement.style.color = 'red';
+}
